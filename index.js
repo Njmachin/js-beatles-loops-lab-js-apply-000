c4565b018ca2c735a775b835dd93f10431f2c530
Nicholas Machin <nicholasjohnmachin@gmail.com>
2019-04-10 13:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBealesPlay(arrayMusicians, arrayInstruments) {
+  arr = [];
+ 
+  for(let i = 0; i < arrayMusicians.length; i++) {
+    str = arrayMusicians[i] + ' plays ' + arrayInstruments[i];
+    arr.push(str);
+  }
+  return arr;
+}
+
+function johnLennonFacts(arrayFacts) {
+  arr =[];
+  while(arr.length !== arrayFacts.length) {
+    str = arrayFacts[i] + '!!!';
+    arr.push(str);
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(aNum) {
+  arr = []; 
+{  do {
+  num += 1;
+  arr.push("I Love the Beatles!")
+} while (num < 15); return arr
+
+
+}
